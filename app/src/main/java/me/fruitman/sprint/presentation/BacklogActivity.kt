c56d0904dceb5f791f5b8ffee25ac9dc95d0df52
Barry Fruitman <barry.fruitman.me>
2023-12-29 13:25:22 -0500

    Added + button to BacklogActivity

--- app/src/main/java/me/fruitman/sprint/presentation/BacklogActivity.kt
+++ app/src/main/java/me/fruitman/sprint/presentation/BacklogActivity.kt
@@ -1,21 +1,24 @@
 package me.fruitman.sprint.presentation
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import me.fruitman.sprint.R
+import me.fruitman.sprint.databinding.ActivityBacklogBinding
 import me.fruitman.sprint.databinding.ActivityEditTaskBinding
 import me.fruitman.sprint.domain.entities.Stage
 
 class BacklogActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityEditTaskBinding
+    private lateinit var binding: ActivityBacklogBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityEditTaskBinding.inflate(layoutInflater)
+        binding = ActivityBacklogBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         supportFragmentManager.commit {
@@ -28,6 +31,15 @@ class BacklogActivity : AppCompatActivity() {
 
         supportActionBar?.title = getString(R.string.backlog)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val newTaskButton: FloatingActionButton = binding.fabNewTask
+        newTaskButton.setOnClickListener {
+            startActivity(
+                Intent(this, EditTaskActivity::class.java).apply {
+                    putExtra("taskId", 0)
+                }
+            )
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
